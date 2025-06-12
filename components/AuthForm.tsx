c907c43933493cfebc5afa694ab32ddf5e995c93
areddin409 <areddin409@gmail.com>
2025-06-12 09:47:34 -0500

    Enhance AuthForm component: add full name field for sign-up, improve form structure, and integrate loading state with visual feedback

--- components/AuthForm.tsx
+++ components/AuthForm.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useForm } from 'react-hook-form';
+import { useState } from 'react';
 import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 
@@ -15,46 +16,127 @@ import {
   FormMessage,
 } from '@/components/ui/form';
 import { Input } from '@/components/ui/input';
-
-const formSchema = z.object({ username: z.string().min(2).max(50) });
+import Image from 'next/image';
+import Link from 'next/link';
 
 type AuthFormType = 'sign-in' | 'sign-up';
 
+const authFormSchema = (formType: AuthFormType) => {
+  return z.object({
+    email: z.string().email(),
+    fullName:
+      formType === 'sign-up'
+        ? z.string().min(2).max(50)
+        : z.string().optional(),
+  });
+};
+
 const AuthForm = ({ type }: { type: AuthFormType }) => {
-  // 1. Define your form.
+  const [isLoading, setIsLoading] = useState(false);
+  const [errorMessage, setErrorMessage] = useState<string>('');
+
+  const formSchema = authFormSchema(type);
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
-    defaultValues: { username: '' },
+    defaultValues: {
+      fullName: '',
+      email: '',
+    },
   });
 
-  // 2. Define a submit handler.
   function onSubmit(values: z.infer<typeof formSchema>) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
     console.log(values);
   }
   return (
-    <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
-        <FormField
-          control={form.control}
-          name="username"
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Username</FormLabel>
-              <FormControl>
-                <Input placeholder="shadcn" {...field} />
-              </FormControl>
-              <FormDescription>
-                This is your public display name.
-              </FormDescription>
-              <FormMessage />
-            </FormItem>
+    <>
+      <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="auth-form">
+          <h1 className="form-title">
+            {type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+          </h1>
+          {type === 'sign-up' && (
+            <FormField
+              control={form.control}
+              name="fullName"
+              render={({ field }) => (
+                <FormItem>
+                  <div className="shad-form-item">
+                    <FormLabel className="shad-form-label">Full Name</FormLabel>
+
+                    <FormControl>
+                      <Input
+                        placeholder="Enter your full name"
+                        className="shad-input"
+                        {...field}
+                      />
+                    </FormControl>
+                  </div>
+
+                  <FormMessage className="shad-form-message" />
+                </FormItem>
+              )}
+            />
           )}
-        />
-        <Button type="submit">Submit</Button>
-      </form>
-    </Form>
+
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <div className="shad-form-item">
+                  <FormLabel className="shad-form-label">Email</FormLabel>
+
+                  <FormControl>
+                    <Input
+                      placeholder="Enter your email"
+                      className="shad-input"
+                      {...field}
+                    />
+                  </FormControl>
+                </div>
+
+                <FormMessage className="shad-form-message" />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="form-submit-button"
+            disabled={isLoading}
+          >
+            {type === 'sign-in' ? 'Sign In' : 'Sign Up'}
+
+            {isLoading && (
+              <Image
+                src="/assets/icons/loader.svg"
+                alt="Loader"
+                width={24}
+                height={24}
+                className="ml-2 animate-spin"
+              />
+            )}
+          </Button>
+
+          {errorMessage && <p className="error-message">{errorMessage}</p>}
+
+          <div className="body-2 flex justify-center">
+            <p className="text-light-100">
+              {type === 'sign-in'
+                ? "Don't have an account?"
+                : 'Already have an account?'}
+            </p>
+            <Link
+              href={type === 'sign-in' ? 'sign-up' : 'sign-in'}
+              className="font-medium text-brand ml-1"
+            >
+              {type === 'sign-in' ? 'Sign Up' : 'Sign In'}
+            </Link>
+          </div>
+        </form>
+      </Form>
+
+      {/* OTP Verification */}
+    </>
   );
 };
 
